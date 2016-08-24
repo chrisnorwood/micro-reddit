04b2a05bf8cab32f855b7daf89286055a5cf11ab
Chris Norwood <cnorwood92@gmail.com>
2016-08-24 15:54:10 -0700

    Validate Post title and body for presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :username, presence: true, length: { maximum: 50 },
                        uniqueness: { case_sensitive: false }
   validates :email,    presence: true, length: { maximum: 255 },
                        format: { with: /.+@.+\..+/i }
   validates :password, presence: true
-
-  has_many :posts
 end
