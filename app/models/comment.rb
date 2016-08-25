7116a56fdefdae9e705d9da5e6ba48d37984fea9
Chris Norwood <cnorwood92@gmail.com>
2016-08-24 17:42:20 -0700

    Create Comment model with validations, associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
-class Post < ApplicationRecord
+class Comment < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  belongs_to :post
 
-  validates :title,   presence: true
   validates :body,    presence: true
   validates :user_id, presence: true
+  validates :post_id, presence: true
 end
